a804618f802ce5e4bff20456e8d04ee551bc9ad4
Michael Tulmen <michaeltulmen@gmail.com>
2022-07-18 14:53:36 -0500

    finalized jest testing

--- photo-port/src/components/About/__tests__/index.test.js
+++ photo-port/src/components/About/__tests__/index.test.js
@@ -1,19 +1,18 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import About from "..";
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
-describe("About component", () => {
-  // First Test
-  it("renders", () => {
+
+describe('About component', () => {
+  it('renders', () => {
     render(<About />);
   });
-
-  //Second Test
-  it("matches snapshot DOPM node structure", () => {
-    //render About
+  
+  it('matches snapshot DOM node structure', () => {
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
